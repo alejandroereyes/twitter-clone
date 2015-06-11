5031d11a29f95c28f750e777db59a9f937c085d9
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-10 22:54:34 -0500

    when new tweet saved, msg count for user will go up 1

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -29,6 +29,17 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
     if @tweet.save
+      if User.exists?(user_name: params[:tweet][:user_name])
+        update_user = User.find_by(user_name: params[:tweet][:user_name])
+        user_current_msg_count = update_user.message_count
+        update_user.message_count = (user_current_msg_count + 1)
+        update_user.save
+      else
+        new_user = User.new
+        new_user.user_name = params[:tweet][:user_name]
+        new_user.message_count = 1
+        new_user.save
+      end
       redirect_to @tweet
     else
       render :new
