0b2c4b15658649a11af26880063696ef2fefe5b2
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-10 19:41:19 -0500

    added route for tweet, updated tweet controller w/ CRU, need D

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,33 @@
 class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.create(tweet_params)
+    if @tweet.save
+      redirect_to @tweet
+    else
+      render :new
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:user_name, :message)
+  end
 end
